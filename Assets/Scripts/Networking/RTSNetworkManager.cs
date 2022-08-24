9ead53347f343d659590276d900f81f84e9a20d3
resh7007 <resh87@mail.ru>
2022-08-24 10:07:46 +0600

    created lobby

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,15 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class RTSNetworkManager : NetworkManager
 {
 
   [SerializeField] private GameObject unitSpawnerPrefab=null;
   [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
-   public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+
+  public static event Action ClientOnConnected;
+  public static event Action ClientOnDisconnected;
+
+
+  public override void OnClientConnect(NetworkConnection conn)
+  {
+      base.OnClientConnect(conn);
+      ClientOnConnected?.Invoke();
+  }
+
+  public override void OnClientDisconnect(NetworkConnection conn)
+  {
+      base.OnClientDisconnect(conn);
+      ClientOnDisconnected?.Invoke();
+
+  }
+
+  public override void OnServerAddPlayer(NetworkConnectionToClient conn)
    {
       base.OnServerAddPlayer(conn);
 
@@ -19,10 +39,10 @@ public class RTSNetworkManager : NetworkManager
           Random.Range(0f,1f),
           Random.Range(0f,1f)));
       
-      GameObject unitSpawnerInstance= Instantiate(unitSpawnerPrefab, conn.identity.transform.position,
-          conn.identity.transform.rotation);
-     
-      NetworkServer.Spawn(unitSpawnerInstance, conn);
+      // GameObject unitSpawnerInstance= Instantiate(unitSpawnerPrefab, conn.identity.transform.position,
+      //     conn.identity.transform.rotation);
+      //
+      // NetworkServer.Spawn(unitSpawnerInstance, conn);
    }
 
    public override void OnServerSceneChanged(string sceneName)
